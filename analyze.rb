2062cd008e55f6d5752c3dfe5147f0cd0f3f00bf
Griffith Chaffee <griffithchaffee@gmail.com>
2025-10-21 00:32:24 -0600

    week 8

--- analyze.rb
+++ analyze.rb
@@ -19,11 +19,25 @@ class Array
 end
 
 csv = CSV.parse(File.read("players.csv"), headers: true)
+players = csv.map(&:to_hash)
 games_by_player = csv.map(&:to_hash).group_by { |h| h["Player"] }
+games_by_player.select! do |player, hashes|
+  next if hashes.size == 0
+  hash = hashes.first
+  # Ben White, Matt O'Riley
+  next if %w[ 04qfq 04e1e ].include?(hash["ID"])
+  next if hashes.sum { |h| h["Minutes Played"].to_f } <= 300
+  #next if hash["Owner"] != "FA"
+  true
+end
+
+puts
+puts "========= TRENDING UP ========="
 results = {}
 games_by_player.each do |player, hashes|
-  #next if !hashes.first || hashes.first["Owner"] != "FA"
+  next if !hashes.first || hashes.first["Owner"] != "FA"
   hashes.each do |hash|
+    #next if %w[ 04qfq 04e1e ].include?(hash["ID"])
     next if hash["Minutes Played"].to_i <= 30
     results[player] ||= []
     results[player] << hash["Fantasy Points"].to_f
@@ -35,20 +49,89 @@ results.each do |player, fpts|
   next if fpts.size <= 2
   trends[player] = fpts.trend_line
 end
-
-trends.sort_by { |k,v| -v[:slope] }.each do |player, trend|
+trends.sort_by { |k,v| -v[:slope] }.last(40).each do |player, trend|
   puts "#{player.ljust(20)} - #{trend[:slope].round(1) * -1}"
 end
 
-sums = {}
+puts
+puts "========= Defensive Points ========="
+results.clear
 games_by_player.each do |player, hashes|
   hashes.each do |hash|
     next if !["D", "D,M"].include?(hash["Position"])
-    sums[hash["Opponent"]] ||= 0
-    sums[hash["Opponent"]] += hash["Fantasy Points"].to_f
+    results[hash["Opponent"]] ||= 0
+    results[hash["Opponent"]] += hash["Fantasy Points"].to_f
   end
 end
 
-sums.sort_by { |k,v| -v }.each do |k,v|
+results.sort_by { |k,v| -v }.each do |k,v|
+  puts "#{k}: #{v}"
+end
+
+puts
+puts "========= Mid Points ========="
+results.clear
+games_by_player.each do |player, hashes|
+  hashes.each do |hash|
+    next if !["M", "M,F", "D,M"].include?(hash["Position"])
+    results[hash["Opponent"]] ||= 0
+    results[hash["Opponent"]] += hash["Fantasy Points"].to_f
+  end
+end
+
+results.sort_by { |k,v| -v }.each do |k,v|
+  puts "#{k}: #{v}"
+end
+
+
+puts
+puts "========= No G/A Fpts ========="
+results.clear
+games_by_player.each do |player, hashes|
+  fpts = 0.0
+  mins = 0.0
+  name = [player.ljust(30, " ")]
+  hashes.each do |hash|
+    name.unshift(hash["Position"].ljust(3, " ")) if name.size < 4
+    name.unshift(hash["Team"]) if name.size < 4
+    name.unshift(hash["Owner"].ljust(15, " ")) if name.size < 4
+    next if ["D", "D,M"].include?(hash["Position"])
+    fpts += hash["Fantasy Points"].to_f
+    fpts -= 10 * hash["Goals"].to_f
+    fpts -= 7 * hash["Assists (Official)"].to_f
+    fpts -= 7 * hash["Assists (Fantasy)"].to_f
+    mins += hash["Minutes Played"].to_f
+  end
+  next if mins <= 400
+  results[name.join(" - ")] = fpts / mins
+end
+
+results.sort_by { |k,v| -v }.first(40).each do |k,v|
+  puts "#{k}: #{v}"
+end
+
+puts
+puts "========= Curve Fpts ========="
+results.clear
+games_by_player.each do |player, hashes|
+  fpts = []
+  mins = 0.0
+  name = [player.ljust(30, " ")]
+  hashes.each do |hash|
+    name.unshift(hash["Position"].ljust(3, " ")) if name.size < 4
+    name.unshift(hash["Team"]) if name.size < 4
+    name.unshift(hash["Owner"].ljust(15, " ")) if name.size < 4
+    fpts.push(hash["Fantasy Points"].to_f)
+    mins += hash["Minutes Played"].to_f
+  end
+  next if mins <= 400
+  fpts.delete(fpts.max)
+  fpts.delete(fpts.min)
+  fpts = fpts.sum
+  next if fpts <= 0
+  results[name.join(" - ")] = fpts / mins
+end
+
+results.sort_by { |k,v| -v }.first(40).each do |k,v|
   puts "#{k}: #{v}"
 end
